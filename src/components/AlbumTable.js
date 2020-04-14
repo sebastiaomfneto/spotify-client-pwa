35fedb850b61b35ac827f3d8a3d524faf4b734ac
Sebastião Neto <smfnto@gmail.com>
2020-04-14 14:21:07 -0300

    Created component AlbumTable

---
+++ src/components/AlbumTable.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Table from "../components/Table";
+
+import { buildImageSrc, buildArtist, buildAvailability } from "../helpers";
+
+export default function AlbumTable({ albums }) {
+  return (
+    <Table columns={["", "Image", "Name", "Artist", "Availability"]}>
+      {albums.map((album) => (
+        <tr key={album.id}>
+          <td></td>
+          <td>
+            <img
+              src={buildImageSrc(album.images)}
+              alt="album"
+              width="20px"
+              height="20px"
+            />
+          </td>
+          <td>
+            <Link
+              to={{
+                pathname: `/albums/${album.id}`,
+                state: { album },
+              }}
+            >
+              {album.name}
+            </Link>
+          </td>
+          <td>{buildArtist(album.artists)}</td>
+          <td>{buildAvailability(album.available_markets)}</td>
+        </tr>
+      ))}
+    </Table>
+  );
+}
--- src/containers/AlbumList.js
+++ src/containers/AlbumList.js
@@ -1,43 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import Container from "../components/Container";
-import Table from "../components/Table";
-
-import { buildImageSrc, buildArtist, buildAvailability } from "../helpers";
+import AlbumTable from "../components/AlbumTable";
 
 export default function AlbumList() {
   const albums = [];
 
   return (
     <Container title="Albums">
-      <Table columns={["", "Image", "Name", "Artist", "Availability"]}>
-        {albums.map((album) => (
-          <tr key={album.id}>
-            <td></td>
-            <td>
-              <img
-                src={buildImageSrc(album.images)}
-                alt="album"
-                width="20px"
-                height="20px"
-              />
-            </td>
-            <td>
-              <Link
-                to={{
-                  pathname: `/albums/${album.id}`,
-                  state: { album },
-                }}
-              >
-                {album.name}
-              </Link>
-            </td>
-            <td>{buildArtist(album.artists)}</td>
-            <td>{buildAvailability(album.available_markets)}</td>
-          </tr>
-        ))}
-      </Table>
+      <AlbumTable albums={albums} />
     </Container>
   );
 }
