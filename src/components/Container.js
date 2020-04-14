bbb25430e0dcea47e169deecbe53266c253ec5e2
Sebastião Neto <smfnto@gmail.com>
2020-04-14 12:07:15 -0300

    Ajustments on component Container

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,10 +2,15 @@ import React from "react";
 
 import "./Container.scss";
 
-export default function Container({ title, subtitle, children }) {
+export default function Container({ imageSrc, title, subtitle, children }) {
   return (
     <div className="Container">
-      <h1>{title}</h1>
+      <div>
+        {imageSrc ? (
+          <img src={imageSrc} alt="container" width="150px" height="150px" />
+        ) : null}
+        <h1>{title}</h1>
+      </div>
       {subtitle ? <h2>{subtitle}</h2> : null}
       {children}
     </div>
