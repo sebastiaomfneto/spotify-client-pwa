d181b4616d756852ba0c4e3f9154ae2ee90e94b6
Sebastião Neto <smfnto@gmail.com>
2020-04-17 09:35:00 -0300

    Created component Routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Redirect,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.scss";
 
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 
-import Signin from "./pages/Signin";
-import ArtistItem from "./pages/ArtistItem";
-import ArtistList from "./pages/ArtistList";
-import AlbumItem from "./pages/AlbumItem";
-import AlbumList from "./pages/AlbumList";
-import TrackList from "./pages/TrackList";
+import Routes from "./Routes";
 
 export default function App() {
   return (
@@ -27,27 +17,7 @@ export default function App() {
         <main>
           <Header />
           <div className="container">
-            <Switch>
-              <Redirect exact from="/" to="/artists" />
-              <Route path="/signin">
-                <Signin />
-              </Route>
-              <Route path="/artists/:id">
-                <ArtistItem />
-              </Route>
-              <Route path="/artists">
-                <ArtistList />
-              </Route>
-              <Route path="/albums/:id">
-                <AlbumItem />
-              </Route>
-              <Route path="/albums">
-                <AlbumList />
-              </Route>
-              <Route path="/tracks">
-                <TrackList />
-              </Route>
-            </Switch>
+            <Routes />
           </div>
         </main>
       </div>
---
+++ src/Routes.js
@@ -0,0 +1,34 @@
+import { Switch, Redirect, Route } from "react-router-dom";
+
+import Signin from "./pages/Signin";
+import ArtistItem from "./pages/ArtistItem";
+import ArtistList from "./pages/ArtistList";
+import AlbumItem from "./pages/AlbumItem";
+import AlbumList from "./pages/AlbumList";
+import TrackList from "./pages/TrackList";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/artists" />
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/artists/:id">
+        <ArtistItem />
+      </Route>
+      <Route path="/artists">
+        <ArtistList />
+      </Route>
+      <Route path="/albums/:id">
+        <AlbumItem />
+      </Route>
+      <Route path="/albums">
+        <AlbumList />
+      </Route>
+      <Route path="/tracks">
+        <TrackList />
+      </Route>
+    </Switch>
+  );
+}
