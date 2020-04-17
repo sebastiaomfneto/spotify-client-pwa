1bc551b64b993370b1ba429dcdef2d56d71c6288
Sebastião Neto <smfnto@gmail.com>
2020-04-17 12:15:37 -0300

    Adjustments on SearchContext

--- src/contexts/SearchContext.js
+++ src/contexts/SearchContext.js
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
 
 const SearchContext = React.createContext();
 
 export default SearchContext;
 
-function getSearchFromLocation(location) {
-  const { search } = location.search
-    .replace(/\?/i, "")
-    .split(/&/i)
-    .map((i) => i.split(/=/i))
-    .reduce((ag, [k, v]) => ({ ...ag, [k]: v }), {});
-
-  return decodeURI(search);
-}
-
 export function SearchContextProvider({ children }) {
-  const history = useHistory();
-  const initialSearch = getSearchFromLocation(history.location);
-
-  const [search, setSearch] = useState(initialSearch);
-
-  useEffect(() => {
-    history.replace({
-      ...history.location,
-      search: `search=${search}`,
-    });
-  }, [search, history]);
+  const [search, setSearch] = useState("");
 
   return (
     <SearchContext.Provider
