aa2fbd35b608c830f72a6a0ef9dbfe75427c2f3f
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-08-19 20:37:36 -0300

    Refactored helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,31 @@
+/**
+ * Build image src attribute
+ *
+ * @param {object[]} images
+ * @returns {string | undefined}
+ */
 export function buildImageSrc([image] = []) {
   return image?.url;
+}
+
+let timeoutId;
+
+/**
+ * Call function debounced
+ *
+ * @param {Function} fn
+ * @param {number} timeout
+ */
+export function debounce(fn, timeout) {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+
+  timeoutId = setTimeout(fn, timeout);
+}
+
+export class NotImplementedError extends Error {
+  constructor() {
+    super('Not implemented');
+  }
 }
