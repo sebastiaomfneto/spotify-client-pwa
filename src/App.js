d18a31b9b2a7659314838a44e5b88e722c84aa57
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-08-19 20:38:26 -0300

    Refactored App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./App.scss";
+import React, { lazy, Suspense } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
 
-import { Nav } from "./components/Nav";
-import { Header } from "./components/Header";
+import { AppLayout, Loading } from './components';
+import {
+  AuthenticationContextProvider,
+  SearchContextProvider,
+  FavoriteContextProvider,
+} from './contexts';
 
-import { Providers } from "./Providers";
-import { Routes } from "./Routes";
+const AlbumRoutes = lazy(() =>
+  import('./features/Album').then((m) => ({ default: m.AlbumRoutes }))
+);
+const ArtistRoutes = lazy(() =>
+  import('./features/Artist').then((m) => ({ default: m.ArtistRoutes }))
+);
+const SigninRoutes = lazy(() =>
+  import('./features/Signin').then((m) => ({ default: m.SigninRoutes }))
+);
+const TrackRoutes = lazy(() =>
+  import('./features/Track').then((m) => ({ default: m.TrackRoutes }))
+);
 
 export default function App() {
   return (
-    <Router>
-      <Providers>
-        <div className="App">
-          <div className="nav-section">
-            <Nav />
-          </div>
-          <main>
-            <Header />
-            <div className="container">
-              <Routes />
-            </div>
-          </main>
-        </div>
-      </Providers>
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <AuthenticationContextProvider>
+        <SearchContextProvider>
+          <FavoriteContextProvider>
+            <AppLayout>
+              <Switch>
+                <Redirect exact from="/" to="/artists" />
+                <Route path="/albums" component={AlbumRoutes} />
+                <Route path="/artists" component={ArtistRoutes} />
+                <Route path="/signin" component={SigninRoutes} />
+                <Route path="/tracks" component={TrackRoutes} />
+              </Switch>
+            </AppLayout>
+          </FavoriteContextProvider>
+        </SearchContextProvider>
+      </AuthenticationContextProvider>
+    </Suspense>
   );
 }
