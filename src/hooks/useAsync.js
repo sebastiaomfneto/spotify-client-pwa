1964708b734ae15d99856b8714ee1070c5873ea6
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-08-19 23:43:26 -0300

    Adjusts on hook useAsync

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -1,25 +1,19 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export function useAsync(fn, initialData) {
   const [data, setData] = useState(initialData);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
 
-  const mounted = useRef();
-
   const perform = useCallback(
     (...args) => {
       setLoading(true);
       Promise.resolve(fn(...args))
         .then((resultData) => {
-          if (mounted.current) {
-            setData(resultData);
-          }
+          setData(resultData);
         })
         .catch((resultError) => {
-          if (mounted.current) {
-            setError(resultError);
-          }
+          setError(resultError);
         })
         .finally(() => {
           setLoading(false);
@@ -27,14 +21,6 @@ export function useAsync(fn, initialData) {
     },
     [fn]
   );
-
-  useEffect(() => {
-    mounted.current = true;
-
-    return () => {
-      mounted.current = false;
-    };
-  }, []);
 
   return {
     data,
