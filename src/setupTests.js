a46dd8e0a46f2c79307e817607a52a9df544bb45
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-08-30 20:42:52 -0300

    Added on setupTest localStorage mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom/extend-expect';
+
+process.env.REACT_APP_SPOTIFY_CLIENT_ID = 'TEST_SPOTIFY_CLIENT_ID';
+
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+  },
+});
